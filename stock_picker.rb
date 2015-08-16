a600ceffd783e6f181c5e264aa9c1e1a45e7d0d4
Nicholas Papayiannakis <Nikos@X.local>
2015-08-16 12:15:40 -0400

    Initial commit

---
+++ stock_picker.rb
@@ -0,0 +1,20 @@
+def stock_picker(array_of_prices)
+    highest_profit, current_profit, buy_price, sell_price, buy_day, sell_day = 0
+    array_of_prices.each_index do |day|
+        current_sell_price = array_of_prices[day]
+        current_buy_price = array_of_prices.first(day).min
+        if current_sell_price != array_of_prices.first
+            current_profit = current_sell_price - current_buy_price
+            if current_profit > highest_profit
+                buy_price = current_buy_price
+                sell_price = current_sell_price
+                buy_day = array_of_prices.rindex(buy_price)
+                sell_day = array_of_prices.rindex(sell_price)
+                highest_profit = sell_price - buy_price
+            end
+        end
+    end
+    puts "buy on day #{buy_day} for $#{buy_price} | sell day #{sell_day} for $#{sell_price}"
+end
+
+stock_picker([2,3,46,6,4,1,5])
