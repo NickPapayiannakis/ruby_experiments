69c2d93197da95cfdfa0afd8a72588d46dec78dd
Nicholas Papayiannakis <Nikos@X.local>
2015-08-19 13:48:51 -0400

    refactored for clarity

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,48 +1,6 @@
-=begin
-	
-PROBLEM: Build a method #bubble_sort that takes an array and returns a sorted array. 
-It must use the bubble sort methodology. 
-
-CONSTRAINTS: 
-
-Must be written in Ruby
-Must not use any pre-defined sort method from ruby library
-Must sort an unknown number of array elements
-Must return results as new array
-Must make passes comparing 2 elements at a time
-
-RESTATE THE PROBLEM
-
-Like in the military, people are lined up in single file.  People swap tallest to shortest
-going through the file repeatedly until the line is sorted.
-
-REDUCTION
-
-DONE 1. Define a method called bubble_sort which accepts and array as an argument
-DONE 2. Find a way to loop through the array
-3. Determine a way to compare two array elements to each other.
-4. Find a way to store the results in a new array and iterate on it in the next pass
-5. Find a way to keep track of progress through the loop.
-6. Find a way to terminate the loop once the sorted condition is met
-7. Determine how to output the results to screen after finished. 
-
-PSEUDO-CODE
-
-bubble_sort(array)
-	loop through each element of array until array.none?(x <=> y == 0 || 1)
-		keep track of iteration?
-		compare each element to the one after it.
-		if element[left] < element[right]
-			element[left], element[right] = element[right], element[left]
-		end
-end
-	
-=end
-
 def bubble_sort(array)
-	finished = false
 
-	until finished
+	loop do
 		swap_count = 0
 		
 		array.each_index do |index| 
@@ -52,13 +10,9 @@ def bubble_sort(array)
 				swap_count += 1	
 			end
 		end
-
-		if (swap_count == 0)
-			finished = true
-		end
+		break if swap_count == 0
 	end
 	p array
 end
-
 
 bubble_sort([4,3,78,2,0,2])
