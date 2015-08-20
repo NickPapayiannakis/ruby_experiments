efc7e68978d76746221bdccf1fc2455dd64549ed
Nicholas Papayiannakis <Nikos@X.local>
2015-08-19 20:31:49 -0400

    Added bubble sort method with block

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,4 @@
+#Bubble sort script whithout block as argument
 def bubble_sort(array)
 
 	loop do
@@ -16,3 +17,27 @@ def bubble_sort(array)
 end
 
 bubble_sort([4,3,78,2,0,2])
+
+
+#Bubble sort script with a block as argument
+def bubble_sort_by(array)
+
+	loop do
+		swap_count, i = 0, 0
+		
+		while i < array.length - 1 
+			
+			if yield(array[i], array[i + 1]) > 0
+				array[i], array[i + 1] = array[i + 1], array[i]
+				swap_count += 1	
+			end
+			i += 1
+		end
+		break if swap_count == 0	
+	end
+	array
+end
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+   left.length - right.length
+ end
