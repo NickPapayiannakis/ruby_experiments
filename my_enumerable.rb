9da91cb345a0c741e52499ba2390acc03c9031c1
Nicholas Papayiannakis <Nikos@X.local>
2015-08-23 18:14:48 -0400

    added new methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,4 +1,4 @@
-# My Enumerators
+# My Enumerable
 
 module Enumerable
 
@@ -12,7 +12,7 @@ module Enumerable
 
 	def my_each_with_index
 		return self unless block_given?
-		for i in 0 .. length
+		for i in 0 ...length
 			yield(self[i], i)
 		end	
 	end
@@ -20,11 +20,55 @@ module Enumerable
 	def my_select
 		new_array = []
 		return self unless block_given?
-		self.my_each {|i| new_array << i if yield(i)}
+		my_each {|i| new_array << i if yield(i)}
 		new_array
 	end
 
-end
 
+    def my_all?
+	    if block_given?
+	      my_each { |i| return false unless yield(i) }
+	    else
+	      my_each { |i| return false unless i }
+	    end
+	    true
+    end
 
+    def my_any?
+    	if block_given?
+	      my_each { |i| return true unless !yield(i) }
+	    else
+	      my_each { |i| return true unless !i }
+	    end
+	    false
+    end
 
+    def my_none?
+    	if block_given?
+	      my_each { |i| return true unless yield(i) }
+	    else
+	      my_each { |i| return true unless i }
+	    end
+	    false
+    end
+
+    def my_count(num = nil)
+    	count = 0
+    	if block_given?
+	      my_each {|i| count +=1 if yield(i)}
+	    elsif num.nil?
+	    	count = length
+	    else
+	      my_each {|i| count +=1 if i}
+	    end
+	    count
+    end
+
+    def my_map
+		new_array = []
+		return self unless block_given?
+		my_each {|i| new_array << yield(i)}
+		new_array
+	end
+
+end #end of module
